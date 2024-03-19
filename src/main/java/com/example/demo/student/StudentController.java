3e32f55a9ed73f83a1e754dead99f737474d0f10
nsanzimfura-eric <erickykress1@gmail.com>
2024-03-19 16:22:29 +0200

    Dependency injection in play

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 public class StudentController {
     private final StudentService studentServices;
 
+    @Autowired
     public StudentController(StudentService studentServices) {
         this.studentServices = studentServices;
     }
